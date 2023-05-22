d6750585abe7161bf495138f858e9de266d16c78
Bagus Setiyo <bagussp40@gmail.com>
2023-05-22 08:57:48 +0700

    update

--- resources/views/layout/gis.blade.php
+++ resources/views/layout/gis.blade.php
@@ -9,6 +9,7 @@
      integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
      crossorigin=""/>
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+     <link rel="stylesheet" type="text/css" href="{{ asset('/css/info.css') }}">
     <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
      integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM="
      crossorigin=""></script>
@@ -23,8 +24,10 @@
 </body>
 
 <script>
-    var map = L.map('map').setView([-7.3105686,112.7888708], 10.4);
-   
+    var map = L.map('map').setView([-7.3105686,112.7888708], 11);
+    map.createPane('labels');
+
+  
     // Note that difference in the "lyrs" parameter in the URL:
     // Hybrid: s,h;
     // Satellite: s;
@@ -32,32 +35,37 @@
     // Terrain: p;
 
     // layer
-    L.tileLayer('http://{s}.google.com/vt?lyrs=s&x={x}&y={y}&z={z}',{
+    var positron = L.tileLayer('http://{s}.google.com/vt?lyrs=s&x={x}&y={y}&z={z}',{
         maxZoom: 20,
         subdomains:['mt0','mt1','mt2','mt3']
+    }).addTo(map);
+
+    var positronLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png', {
+            attribution: '©OpenStreetMap, ©CartoDB',
+            pane: 'labels'
     }).addTo(map);
 
   
     // marker
     var hospitalIcon = L.icon({
         iconUrl: 'assets/icons/hospital.png',
-        iconSize:     [38, 48], // size of the icon
+        iconSize:     [30, 35], // size of the icon
     });
     var apotekIcon = L.icon({
         iconUrl: 'assets/icons/apotek.png',
         iconSize:     [38, 48], // size of the icon
-    });
-    var puskesmasIcon = L.icon({
-        iconUrl: 'assets/icons/puskesmas.png',
+    })
+    var klinikIcon = L.icon({
+        iconUrl: 'assets/icons/klinik.png',
         iconSize:     [38, 48], // size of the icon
     });
 
     //geojson polygon
     $.getJSON('assets/geojson/map.geojson', function(json){
         function getColor(d){
-            return  d == 3 ? '#800026':
-                    d == 2 ? '#FC4E2A':
-                    d == 1 ? '#FED976': 
+            return  d == 3 ? '#022D36':
+                    d == 2 ? '#022D36':
+                    d == 1 ? '#022D36': 
                             '#E31A1C';
         }
 
@@ -86,7 +94,7 @@
 
    
     //geojson titik
-    $.getJSON('assets/geojson/mape.geojson', function (data) {
+    $.getJSON('assets/geojson/mapTitik.geojson', function (data) {
         function daerahFilter(feature) {
             if("{{ $daerah }}" == "semua"){
                 return true
@@ -100,8 +108,8 @@
                 return hospitalIcon;
             }else if(d == "apotek"){
                 return apotekIcon;
-            }else if(d == "puskesmas"){
-                return puskesmasIcon;
+            }else if(d == "klinik"){
+                return klinikIcon;
             }
         }
 
@@ -127,6 +135,25 @@
         }).addTo(map);
     });
 
+
+    //info
+    var info = L.control();
+
+    info.onAdd = function (map) {
+        this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+        this.update();
+        return this._div;
+    };
+
+    // method that we will use to update the control based on feature properties passed
+    info.update = function (props) {
+        this._div.innerHTML = '<h4>Keterangan </h4>' + 
+            "<img src='assets/icons/hospital.png'</img>" + '<a> Rumah Sakit</a>' + "<br>" +
+            "<img src='assets/icons/klinik.png'</img>" + '<a> Klinik</a>' + "<br>" +
+            "<img src='assets/icons/apotek.png'</img>" + '<a> Apotek</a>';
+    };
+
+    info.addTo(map);
 
 </script>
 </html>
